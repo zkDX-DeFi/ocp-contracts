3f365c9a933ece4940ad375ad0f4b947f1a24b00
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-26 13:58:30 +0800

    test: check OT.FUNC => assetURIs

--- test/OCPOmniToken.test.ts
+++ test/OCPOmniToken.test.ts
@@ -9,10 +9,11 @@ describe("OT", async () => {
     let user1: any,
         owner: any,
         usdc: any,
+        ocpTokenManager: any,
         ot : any;
 
     beforeEach(async () => {
-        ({owner,user1} = await deployFixture());
+        ({owner,user1, ocpTokenManager} = await deployFixture());
         usdc = await deployNew("Token", ["USDC", 18, 0, 0, 0]);
         ot = await deployNew("OmniToken", [
             "OmniToken",
@@ -54,5 +55,13 @@ describe("OT", async () => {
         await expect(ot.connect(validUser).burn(
             owner.address, 2000)).to.be.ok;
 
+    });
+
+    it("check OT.FUNC => assetURIs", async () => {
+        const tm = ocpTokenManager;
+        const {ot} = await getOT_ZERO(owner, 1000, tm.address);
+        expect(await ot.tokenManager()).eq(tm.address);
+
+        console.log(`${await ot.assetURIs()}`);
     });
 });
