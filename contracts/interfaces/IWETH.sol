dc875831ab5ae863855171662c475e1bcaf39402
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-27 11:55:02 +0800

    refactor: IWETH

--- contracts/interfaces/IWETH.sol
+++ contracts/interfaces/IWETH.sol
@@ -1,11 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 interface IWETH is IERC20 {
-
     function deposit() external payable;
-
     function withdraw(uint) external;
 }
