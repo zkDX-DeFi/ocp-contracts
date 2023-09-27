25bc9e4c9aa8506a924e6b5b3e71d3650a46b534
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-27 11:10:45 +0800

    refactor: remove unused code

--- contracts/interfaces/IOCPOmniTokenManager.sol
+++ contracts/interfaces/IOCPOmniTokenManager.sol
@@ -1,27 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-
 import "../libraries/Structs.sol";
-
 interface IOCPOmniTokenManager {
-
     function omniTokens(address _srcToken, uint16 _chainId) external view returns (address token);
-
     function sourceTokens(address _omniToken, uint16 _chainId) external view returns (address token);
-
     function createOmniToken(Structs.MintObj memory _mintParams, address _lzEndpoint, uint16 _srcChainId) external returns (address token);
-
     function omniMint(address _srcToken, uint16 _dstChainId, uint256 _amount, address _to) external returns (address token);
-
     function omniBurn(address _omniToken, uint256 _amount, address _from) external;
-
-//    function getAssetURIs(uint16[] calldata _chainIds, address[] calldata _pools) external view returns (string[] memory assetURIs);
-
-//    function addSourceToken(
-//        address _omniToken,
-//        uint16 _srcChainId,
-//        address _srcToken,
-//        address _srcPool,
-//        string calldata _symbolCheck
-//    ) external;
 }
