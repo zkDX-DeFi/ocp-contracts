021af30be5306d3ab07c027ebdda68cf0365515a
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-27 11:54:18 +0800

    refactor: IOT

--- contracts/interfaces/IOmniToken.sol
+++ contracts/interfaces/IOmniToken.sol
@@ -1,25 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../libraries/Structs.sol";
-
-interface IOmniToken is IERC20, IERC20Metadata {
-
+interface IOmniToken is IERC20 {
     function mint(address account, uint256 amount) external;
-
     function burn(address account, uint256 amount) external;
-
-    function updateSourcePool(uint16 _srcChainId, address _srcPool) external;
-
-    function setTrustedRemote(uint16 _remoteChainId, bytes calldata _path) external;
-
-    function lzReceive(
-        uint16 _srcChainId,
-        bytes calldata _srcAddress,
-        uint64 _nonce,
-        bytes calldata _payload
-    ) external;
-
 }
