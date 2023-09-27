3654693c67ddec80188e241b875c3eb284a1b888
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-27 11:59:51 +0800

    refactor: OCPF.sol

--- contracts/OCPPoolFactory.sol
+++ contracts/OCPPoolFactory.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-
 import "./interfaces/IOCPoolFactory.sol";
 import "./entity/OCPool.sol";
-
 contract OCPoolFactory is IOCPoolFactory {
     mapping(address => address) public override getPool; // srcToken -> pool
-
     function createPool(address _token) external override returns (address pool){
         require(address(getPool[_token]) == address(0x0), "OCPPoolFactory: Pool already exists");
 
