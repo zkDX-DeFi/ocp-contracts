30c6affa57e0bef88086567451a98efc99375ebb
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-27 08:40:34 +0800

    test: check OCPTM.FUNC => requestAddSourceTokens() && code coverage = 100%

--- test/OCPTokenManager.test.ts
+++ test/OCPTokenManager.test.ts
@@ -12,10 +12,10 @@ describe("OCPTM", async () => {
         ocpTokenManager: any
 
     beforeEach(async () => {
-        ({owner,user1, ocpTokenManager} = await deployFixture());
+        ({owner, user1, ocpTokenManager} = await deployFixture());
         usdc = await deployNew("Token", ["USDC", 18, 0, 0, 0]);
     });
-    it("check OCPTM.FUNC => createToken", async() => {
+    it("check OCPTM.FUNC => createToken", async () => {
         const tm = ocpTokenManager;
 
         const _mintParams = getMintParams_ZERO;
@@ -25,21 +25,21 @@ describe("OCPTM", async () => {
         await tm.createToken(_mintParams, _lzEndpoint, _srcChainId);
     });
 
-    it("check OCPTM.FUNC => updateRouter", async() => {
+    it("check OCPTM.FUNC => updateRouter", async () => {
         const tm = ocpTokenManager;
 
         expect(await tm.router()).eq(AddressZero);
         const invalidUser = user1;
         const validValue = owner.address;
         await expect(tm.connect(invalidUser)
-                .updateRouter(validValue))
+            .updateRouter(validValue))
             .to.be.revertedWith("Ownable: caller is not the owner");
 
         await expect(tm.updateRouter(validValue)).to.be.ok;
         expect(await tm.router()).eq(validValue)
     });
 
-    it("check OCPTM.FUNC => omniMint", async() => {
+    it("check OCPTM.FUNC => omniMint", async () => {
         const tm = ocpTokenManager;
 
         const _srcToken = usdc;
@@ -57,7 +57,7 @@ describe("OCPTM", async () => {
             .to.be.ok;
     });
 
-    it("check OCPTM.FUNC => omniBurn", async() => {
+    it("check OCPTM.FUNC => omniBurn", async () => {
         const tm = ocpTokenManager;
         const invalidUser = user1;
         const validUser = owner;
@@ -79,7 +79,7 @@ describe("OCPTM", async () => {
         )).to.be.ok;
     });
 
-    it("check OCPTM.FUNC => addSourceToken", async() => {
+    it("check OCPTM.FUNC => addSourceToken", async () => {
         const tm = ocpTokenManager;
         const invalidUser = user1;
         const validUser = owner;
@@ -109,9 +109,9 @@ describe("OCPTM", async () => {
         )).to.be.ok;
     });
 
-    it("check OCPTM.FUNC => getAssetURIs", async() => {
+    it("check OCPTM.FUNC => getAssetURIs", async () => {
         const tm = ocpTokenManager;
-        const _chainIds = [0,0];
+        const _chainIds = [0, 0];
         const _pools = [AddressZero, AddressZero];
 
         console.log(`${await tm.getAssetURIs(
@@ -120,9 +120,21 @@ describe("OCPTM", async () => {
         )}`);
     });
 
-    it("check OCPTM.FUNC => approveSourceTokens", async() => {
+    it("check OCPTM.FUNC => approveSourceTokens", async () => {
         const tm = ocpTokenManager;
         await tm.approveSourceTokens();
+    });
+
+    it("check OCPTM.FUNC => requestAddSourceTokens()", async () => {
+        const tm = ocpTokenManager;
+        const _omniToken = usdc;
+        const _srcChainIds = [0];
+        const _srcTokens = [usdc.address];
+        await tm.requestAddSourceTokens(
+            _omniToken.address,
+            _srcChainIds,
+            _srcTokens
+        );
     })
 
 });
