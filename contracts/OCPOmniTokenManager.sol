627d1f432cba50d94992f835304813d313dd83a3
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-28 14:08:33 +0800

    fix: removed unused code && code coverage = 100%

--- contracts/OCPOmniTokenManager.sol
+++ contracts/OCPOmniTokenManager.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IOCPOmniTokenManager.sol";
 import "./libraries/Structs.sol";
 import "./entity/OmniToken.sol";
