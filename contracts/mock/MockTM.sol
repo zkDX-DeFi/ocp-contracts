6d08cc2131a3ca50050b21a295ecb0fe7a0d10b1
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-28 11:25:03 +0800

    fix: test contract size

--- contracts/mock/MockTM.sol
+++ contracts/mock/MockTM.sol
@@ -1,6 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+//import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MockTM {
+contract MockTM  {
+    address public router;
+    address[] public omniTokenList;
+    mapping(address => mapping(uint16 => address)) public omniTokens; // srcToken -> srcChainId -> omniToken
+    mapping(address => mapping(uint16 => address)) public sourceTokens; // omniToken -> srcChainId -> srcToken
 
+    function createOmniToken() external returns(address _token) {
+    }
+    function omniMint() external returns(address _token) {
+    }
+    function omniBurn() external returns(address _token) {
+    }
+
+    function requestAddSourceTokens() external {
+    }
+    function approveSourceTokens() external {
+    }
+    function updateRouter(address _router) external {
+        router = _router;
+    }
 }
