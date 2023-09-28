4dd9bc9a00d58c59cb1aca768666e864d3d60884
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-28 13:19:18 +0800

    fix: mockTM.size = 16.155 kb

--- contracts/mock/MockTM.sol
+++ contracts/mock/MockTM.sol
@@ -1,18 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-//import "@openzeppelin/contracts/access/Ownable.sol";
 import "../entity/OmniToken.sol";
 import "../libraries/Structs.sol";
 
 contract MockTM  {
     address public router;
-    address[] public omniTokenList;
+    address public owner;
     mapping(address => mapping(uint16 => address)) public omniTokens; // srcToken -> srcChainId -> omniToken
     mapping(address => mapping(uint16 => address)) public sourceTokens; // omniToken -> srcChainId -> srcToken
 
+    constructor (address _router) {
+        router = _router;
+        owner = msg.sender;
+    }
+    modifier onlyRouter() {
+        require(msg.sender == router, "OCPTokenManager: caller is not the router");
+        _;
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "OCPTokenManager: caller is not the owner");
+        _;
+    }
+
     function createOmniToken(
         Structs.MintObj memory _mintParams,
-        address _lzEndpoint
+        address _lzEndpoint,
+        uint16 _srcChainId
     ) external returns(address _token) {
         OmniToken newToken = new OmniToken{salt: keccak256(abi.encodePacked(address(this)))}(
             _mintParams.name,
@@ -22,8 +35,8 @@ contract MockTM  {
             _lzEndpoint);
 
         _token = address (newToken);
-
-
+        omniTokens[_mintParams.srcToken][_srcChainId] = _token;
+        sourceTokens[_token][_srcChainId] = _mintParams.srcToken;
     }
     function omniMint() external returns(address _token) {
     }
@@ -34,7 +47,7 @@ contract MockTM  {
     }
     function approveSourceTokens() external {
     }
-    function updateRouter(address _router) external {
+    function updateRouter(address _router) external onlyOwner {
         router = _router;
     }
 }
