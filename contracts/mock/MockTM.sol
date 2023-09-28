149d07d6b1e8aae3ad4335ccca569818d68ae9b1
Muller <141995328+FriedrichMuelle@users.noreply.github.com>
2023-09-28 13:11:19 +0800

    test: mockTM.size = 15.983kb

--- contracts/mock/MockTM.sol
+++ contracts/mock/MockTM.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 //import "@openzeppelin/contracts/access/Ownable.sol";
+import "../entity/OmniToken.sol";
+import "../libraries/Structs.sol";
 
 contract MockTM  {
     address public router;
@@ -8,7 +10,20 @@ contract MockTM  {
     mapping(address => mapping(uint16 => address)) public omniTokens; // srcToken -> srcChainId -> omniToken
     mapping(address => mapping(uint16 => address)) public sourceTokens; // omniToken -> srcChainId -> srcToken
 
-    function createOmniToken() external returns(address _token) {
+    function createOmniToken(
+        Structs.MintObj memory _mintParams,
+        address _lzEndpoint
+    ) external returns(address _token) {
+        OmniToken newToken = new OmniToken{salt: keccak256(abi.encodePacked(address(this)))}(
+            _mintParams.name,
+            _mintParams.symbol,
+            _mintParams.amount,
+            _mintParams.to,
+            _lzEndpoint);
+
+        _token = address (newToken);
+
+
     }
     function omniMint() external returns(address _token) {
     }
